184f6b55cb793611a871c5e53d04070504eeca24
steinko <stein_korsveien@msn.com>
2020-03-03 01:14:30 +0100

    Made possible to check out appels and banana

--- src/cucumberTest/java/cucumber/steps/org/steinko/cucumbertutorial/CheckoutSteps.java
+++ src/cucumberTest/java/cucumber/steps/org/steinko/cucumbertutorial/CheckoutSteps.java
@@ -1,30 +1,35 @@
 package org.steinko.cucumbertutorial;
 import cucumber.api.java.en.Given;
+
 import static org.junit.Assert.assertEquals;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import cucumber.api.PendingException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSteps {
 	
 	private  int bananaPrice;
-	private Checkout checkout;
+	private  Map<String,Integer> prices = new  HashMap<String,Integer>();
+	private Checkout checkout = new Checkout();;
 	
-	@Given("the price of a {string} is {int}c")
+	@Given("the price of a {string} is {int} c")
 	public void thePriceOfAIsC(String name, Integer price) {
-	    bananaPrice = price;
+		 prices.put(name,price);
 	}
 
 	@When("I checkout {int} {string}")
 	public void iCheckout(Integer itemCount, String itemName) {
-	    checkout = new Checkout();
-	    checkout.add(itemCount,bananaPrice);
+		Integer price = prices.get(itemName);
+	    checkout.add(itemCount,price);
 	}
 
-	@Then("the total price should be {int}")
-	public void theTotalPriceShouldBeC(Integer total) {
-	   assertEquals(total,checkout.total());
 
+	@Then("the total price should be {int} c")
+	public void theTotalPriceShouldBeC(Integer total) {
+		 assertEquals(total,checkout.total());
 	}
 	
 }
--- src/cucumberTest/resources/features/checkout.feature
+++ src/cucumberTest/resources/features/checkout.feature
@@ -1,8 +1,22 @@
 Feature: Checkout
+
+ Scenario: Two bananas scanned separately 
+    Given the price of a "banana" is 40 c 
+    When I checkout 1 "banana"
+    And I checkout 1 "banana"
+    Then the total price should be 80 c
+    
+ Scenario: A banana and an apple
+    Given the price of a "banana" is 40 c 
+    And the price of a "apple" is 25 c 
+    When I checkout 1 "banana"
+    And I checkout 1 "apple"
+    Then the total price should be 65 c
+    
   Scenario Outline: Checkout a banana
-    Given the price of a "banana" is 40c 
+    Given the price of a "banana" is 40 c 
     When I checkout <count> "banana"
-    Then the total price should be <total>
+    Then the total price should be <total> c
 
     Examples:
           | count | total |
