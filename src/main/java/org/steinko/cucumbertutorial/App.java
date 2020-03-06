851dbff97a0f86b4869326b6927cb99049330dc2
steinko <stein_korsveien@msn.com>
2020-03-06 07:26:24 +0100

    Add Sonarcube

--- src/main/java/org/steinko/cucumbertutorial/App.java
+++ src/main/java/org/steinko/cucumbertutorial/App.java
@@ -4,11 +4,7 @@
 package org.steinko.cucumbertutorial;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
-
+  
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
     }
 }
